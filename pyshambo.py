eb79700b4dc602f94a138a2acaa8406e47c83088
jose-santana7 <santanajcr01@gmail.com>
2021-11-12 19:40:57 -0800

    increased range to include scissors, randomizer still feels lackluster

--- pyshambo.py
+++ pyshambo.py
@@ -22,7 +22,7 @@ class Contestant:
         else: self.compscore += 1
     
     def comp_choice(self, option_list):
-        option =  option_list[random.randrange(0, 2)]
+        option =  option_list[random.randrange(0, 3)]
         return [option.get('name'), option.get('beats'), option.get('loses')]
 
     def user_choice(self, choice, option_list):
